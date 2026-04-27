289e2d1d75254d55f26a1ebe01cd02aeeb314ad0
ziadsghir8-a11y <ziadsghir8@gmail.com>
2026-04-27 17:39:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def check_housing():
             if 'saint leu' in nom_residence or 'saint-leu' in nom_residence:
                 return True
                 
-        return True
+        return False
         
     except Exception as e:
         print(f"❌ Erreur lors de l'analyse de la page : {e}")
